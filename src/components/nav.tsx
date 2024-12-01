35a0333618c57efe16b9c03418cad3ef10ea3ee3
Kenny Tran <kenny.tran@adriveny.com>
2024-12-01 21:35:27 +0100

    feat: extend nav component to include Item and Dropdown

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,30 @@
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import { cn } from "../utils/cn";
+import { Navigation } from "./tabs";
+
+const Item = React.forwardRef<
+  HTMLSpanElement,
+  React.HTMLAttributes<HTMLSpanElement>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <span
+      ref={ref}
+      className={cn("text-sm/6 font-semibold text-gray-900", className)}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+});
+
+Item.displayName = "Item"; // Adding a display name for better debugging in React DevTools
 
 const Left = React.forwardRef<
   HTMLSpanElement,
@@ -35,4 +60,39 @@ const Right = React.forwardRef<
 
 Right.displayName = "Right"; // Adding a display name for better debugging in React DevTools
 
-export { Left, Right };
+const Dropdown: React.FC<{
+  children: React.ReactNode;
+  navigation: Omit<Navigation, "current">[];
+  wrapper: React.ReactElement;
+}> = ({ children, navigation, wrapper }) => {
+  return (
+    <PopoverGroup className="hidden lg:block">
+      <Popover className="relative">
+        <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
+          {children}
+          <ChevronDownIcon
+            aria-hidden="true"
+            className="size-5 flex-none text-gray-400"
+          />
+        </PopoverButton>
+
+        <PopoverPanel
+          transition
+          className="absolute -left-8 top-full z-10 mt-3 w-56 rounded-xl bg-white p-2 shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+        >
+          {navigation.map((item) =>
+            React.cloneElement(
+              wrapper,
+              { key: item.name, href: item.href },
+              <Item>{item.name}</Item>,
+            ),
+          )}
+        </PopoverPanel>
+      </Popover>
+    </PopoverGroup>
+  );
+};
+
+Dropdown.displayName = "Dropdown"; // Adding a display name for better debugging in React DevTools
+
+export { Dropdown, Left, Right };
