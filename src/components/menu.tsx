a017d9b1fcea88cd474f9663ab9aa2de20357362
Kenny Tran <kenny.tran@adriveny.com>
2024-12-02 19:35:54 +0100

    chore: replace typeof function check with 'a' assertion

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -138,7 +138,7 @@ const Dropdown: React.FC<{
         {navigation.map((item) => (
           <DisclosureButton
             key={item.name}
-            as={typeof A === "function" ? A : "a"}
+            as={A ? (A as "a") : "a"}
             href={item.href}
             className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
           >
