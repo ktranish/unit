b744c02c9ded2d611421f9bdfd3ae1a97bc260ae
Kenny Tran <kenny.tran@adriveny.com>
2024-12-02 22:26:25 +0100

    fix: remove w-full class from menu item

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -108,7 +108,7 @@ const Item = React.forwardRef<
     <span
       ref={ref}
       className={cn(
-        "-mx-3 block w-full rounded-lg px-3 py-2.5 text-left text-base/7 font-semibold text-gray-900 hover:bg-gray-50",
+        "-mx-3 block rounded-lg px-3 py-2.5 text-left text-base/7 font-semibold text-gray-900 hover:bg-gray-50",
         className,
       )}
       {...props}
