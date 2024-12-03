8d1314d2f224dc0231aeac5eabd39debd1b858b0
Kenny Tran <kenny.tran@adriveny.com>
2024-12-03 12:59:03 +0100

    feat: add ping component

---
+++ src/components/ping.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { cn } from "../utils/cn";
+
+const Ping = React.forwardRef<
+  HTMLSpanElement,
+  React.HTMLAttributes<HTMLSpanElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <span
+      ref={ref}
+      className={cn(
+        "my-auto size-10 rounded-full border-4 motion-safe:animate-ping",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
+
+Ping.displayName = "Ping"; // Adding a display name for better debugging in React DevTools
+
+export default Ping;
